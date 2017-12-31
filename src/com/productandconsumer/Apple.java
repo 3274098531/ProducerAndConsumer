f52cf7b415f66d73231de08b835b143ea2d270f3
liuwei <3274098531@qq.com>
2017-12-31 14:51:53 +0800

    ""

--- src/com/productandconsumer/Apple.java
+++ src/com/productandconsumer/Apple.java
@@ -4,7 +4,8 @@ public class Apple {
     private String name;
     public void setName(String name) {
     	this.name=name;
-     } 
+    /**/ 
+    } 
 	public String getName() {
 		return name;
 	}
